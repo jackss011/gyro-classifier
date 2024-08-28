8843f87fd30c89bc34b26e93e68fdec2921f582b
Giacomo Agnetti <giacomo.a981@gmail.com>
2024-08-29 00:50:37 +0200

    fixed triplet loss

--- padova/src/train_triplet.py
+++ padova/src/train_triplet.py
@@ -61,8 +61,11 @@ print("num classes:", train_ds.num_classes)
 
 # ========> MODEL <=========
 model = CNN_binary(num_classes).to(device)
+# init_weights(model)
+extract_features = lambda x: model.net(x).reshape(x.size(0), -1)
+
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=5)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=15)
 
 distance_fn = torch.nn.PairwiseDistance(p=2)
 criterion = torch.nn.TripletMarginWithDistanceLoss(distance_function=distance_fn, margin=margin, swap=True, reduction="mean")
@@ -81,9 +84,10 @@ for e in tqdm(range(1, epochs + 1), desc="epochs"):
         anchor, pos, neg = anchor.to(device), pos.to(device), neg.to(device)
 
         optimizer.zero_grad()
-        anchor_out = model.net(anchor)
-        pos_out = model.net(pos)
-        neg_out = model.net(neg)
+        anchor_out = extract_features(anchor)
+        pos_out = extract_features(pos)
+        neg_out = extract_features(neg)
+        assert(anchor_out.ndim == 2)
 
         loss = criterion(anchor_out, pos_out, neg_out)
         loss.backward()
@@ -100,19 +104,31 @@ for e in tqdm(range(1, epochs + 1), desc="epochs"):
         model.eval()
 
         running_loss = []
+        count = 0
+        correct = 0
         for anchor, pos, neg, _, _, _ in tqdm(test_loader, desc="validation (test set)", leave=False):
             anchor, pos, neg = anchor.to(device), pos.to(device), neg.to(device)
 
-            anchor_out = model.net(anchor)
-            pos_out = model.net(pos)
-            neg_out = model.net(neg)
+            anchor_out = extract_features(anchor)
+            pos_out = extract_features(pos)
+            neg_out = extract_features(neg)
 
             loss = val_criterion(anchor_out, pos_out, neg_out)
             running_loss.append(loss.item())
 
+            pos_dist = val_distance_fn(anchor_out, pos_out)
+            neg_dist = val_distance_fn(anchor_out, neg_out)
+
+            count += pos_dist.size(0)
+            correct += torch.sum(pos_dist < neg_dist).item()
+
         mean_loss = np.array(running_loss).mean()
         summary.add_scalar("validation/loss_test", mean_loss, e)
         print(">> validation loss (test set):", mean_loss)
+
+        perc_accuracy = correct / count
+        summary.add_scalar("validation/accuracy", perc_accuracy, e)
+        print(f">> accuracy (test set): {perc_accuracy*100:.1f}")
 
         if mean_loss < loweset_validation_loss:
             loweset_validation_loss = mean_loss
@@ -136,9 +152,9 @@ for e in tqdm(range(1, epochs + 1), desc="epochs"):
 #             for anchor, pos, neg, _, _, _ in tqdm(train_loader, desc="validation (traning set)", leave=False):
 #                 anchor, pos, neg = anchor.to(device), pos.to(device), neg.to(device)
 
-#                 anchor_out = model.net(anchor)
-#                 pos_out = model.net(pos)
-#                 neg_out = model.net(neg)
+#                 anchor_out = model.net(anchor).reshape(anchor.size(0), -1)
+#                 pos_out = model.net(pos).reshape(pos.size(0), -1)
+#                 neg_out = model.net(neg).reshape(neg.size(0), -1)
 
 #                 loss = val_criterion(anchor_out, pos_out, neg_out)
 #                 running_loss.append(loss.item())
